a8cccc629d4ef96f384455ef70459d2876812987
melyymix1 <mgutierrezs2104@alumno.ipn.mx>
2023-03-04 00:53:58 -0600

    Algoritmia

--- 05Algoritmia/js/logica.js
+++ 05Algoritmia/js/logica.js
@@ -1,3 +1,56 @@
+function problema1(){
+    var valores = document.querySelector('#p1-input').value;
+    var split = valores.split(' ').reverse();
+    var resultado = '';
+
+    split.forEach(function (palabras, i){
+
+        if(i != 0 || i != split.length) resultado += ' ';
+        resultado += palabras;
+    });
+
+    document.querySelector('#p1-output').textContent = resultado;
+}
+function problema2()
+{
+    //obtener valores de x
+    var x1 = document.querySelector('#p2--x1').value;
+    var x2 = document.querySelector('#p2--x2').value;
+    var x3 = document.querySelector('#p2--x3').value;
+    var x4 = document.querySelector('#p2--x4').value;
+    var x5 = document.querySelector('#p2--x5').value;
+    //obtener valores de y
+    var y1 = document.querySelector('#p2--y1').value;
+    var y2 = document.querySelector('#p2--y2').value;
+    var y3 = document.querySelector('#p2--y3').value;
+    var y4 = document.querySelector('#p2--y4').value;
+    var y5 = document.querySelector('#p2--y5').value;
+
+    var v1 = [x1, x2, x3, x4, x5];
+    var v2 = [y1, y2, y3, y4, y5];
+
+/*El método sort() ordena los elementos de un arreglo (array) localmente y devuelve el arreglo ordenado. 
+La ordenación no es necesariamente estable. El modo de ordenación por defecto responde a la posición del 
+valor del string de acuerdo a su valor Unicode. */
+    v1 = v1.sort(function (v1, v2)
+    {
+        return v2-v1;
+    });
+
+    v2 = v2.sort(function (v1, v2)
+    {
+        return v2-v1;
+    });
+
+    var resultado = 0;
+
+    for(var i = 0; i < v1.length; i++)
+    {
+        resultado += v1[i] * v2[i];
+    }
+
+    document.querySelector('#p2-output').textContent = 'El mínimo producto escalar es: '+ resultado;
+}
 function problema3()
 {
     var alfabeto = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
@@ -42,8 +95,31 @@ function problema3()
         });
     });
     //vammos a contar la salida 
-    p3_res+= 'Palbras: ' + palabra + '='+ letras_unicas.length +' \n';
+    p3_res+= 'Palabras: ' + palabra + '='+ letras_unicas.length +' \n';
    });
    //imprimir salida
    document.querySelector('#p3-output').textContent =p3_res;
+}
+function borrar1()
+{
+    document.querySelector("#p1-output").textContent="";
+}
+function borrar2()
+{
+    document.querySelector("#p2-output").textContent="";
+    document.getElementById("p2--x1").value="";
+    document.getElementById("p2--x2").value="";
+    document.getElementById("p2--x3").value="";
+    document.getElementById("p2--x4").value="";
+    document.getElementById("p2--x5").value="";
+    document.getElementById("p2--y1").value="";
+    document.getElementById("p2--y2").value="";
+    document.getElementById("p2--y3").value="";
+    document.getElementById("p2--y4").value="";
+    document.getElementById("p2--y5").value="";
+}
+function borrar3()
+{
+    document.querySelector("#p3-output").textContent="";
+ 
 }
